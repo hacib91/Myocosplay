eefa12ce9bd4e780916b6374baeb2b1c58801f8a
Haseeb <haseeb.khalid@epitech.eu>
2016-06-28 11:23:37 +0200

    Initial Commit

---
+++ Myo Cosplay Test api/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Myo Cosplay Test api
+//
+//  Created by haseeb khalid on 28/06/2016.
+//  Copyright © 2016 haseeb khalid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
